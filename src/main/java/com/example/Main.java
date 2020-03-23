5d7623fe34a4a852bc6d3a6f233062f170f2af47
KisaragiEffective <kisaragi.effective@gmail.com>
2020-03-23 09:08:59 +0900

    indent

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -32,57 +32,59 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 @Controller
 @SpringBootApplication
 public class Main {
+    @Value("${spring.datasource.url}")
+    @SuppressWarnings({"unused"})
+    private String dbUrl;
 
-  @Value("${spring.datasource.url}")
-  private String dbUrl;
+    @Autowired
+    @SuppressWarnings({"unused"})
+    private DataSource dataSource;
 
-  @Autowired
-  private DataSource dataSource;
-
-  public static void main(String[] args) throws Exception {
-    SpringApplication.run(Main.class, args);
-  }
-
-  @RequestMapping("/")
-  String index() {
-    return "index";
-  }
-
-  @RequestMapping("/db")
-  String db(Map<String, Object> model) {
-    try (Connection connection = dataSource.getConnection()) {
-      Statement stmt = connection.createStatement();
-      stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
-      stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
-      ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
-
-      ArrayList<String> output = new ArrayList<String>();
-      while (rs.next()) {
-        output.add("Read from DB: " + rs.getTimestamp("tick"));
-      }
-
-      model.put("records", output);
-      return "db";
-    } catch (Exception e) {
-      model.put("message", e.getMessage());
-      return "error";
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Main.class, args);
     }
-  }
 
-  @Bean
-  public DataSource dataSource() throws SQLException {
-    if (dbUrl == null || dbUrl.isEmpty()) {
-      return new HikariDataSource();
-    } else {
-      HikariConfig config = new HikariConfig();
-      config.setJdbcUrl(dbUrl);
-      return new HikariDataSource(config);
+    @RequestMapping("/")
+    @SuppressWarnings({"unused"})
+    String index() {
+        return "index";
     }
-  }
 
+    @RequestMapping("/db")
+    @SuppressWarnings({"unused"})
+    String db(Map<String, Object> model) {
+        try (Connection connection = dataSource.getConnection()) {
+            Statement stmt = connection.createStatement();
+            stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
+            stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
+            try(ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks")) {
+                List<String> output = new ArrayList<>();
+                while (rs.next()) {
+                    output.add("Read from DB: " + rs.getTimestamp("tick"));
+                }
+                model.put("records", output);
+                return "db";
+            }
+        } catch (Exception e) {
+            model.put("message", e.getMessage());
+            return "error";
+        }
+    }
+
+    @Bean
+    public DataSource dataSource() throws SQLException {
+        if (dbUrl == null || dbUrl.isEmpty()) {
+            return new HikariDataSource();
+        } else {
+            HikariConfig config = new HikariConfig();
+            config.setJdbcUrl(dbUrl);
+            return new HikariDataSource(config);
+        }
+    }
 }
